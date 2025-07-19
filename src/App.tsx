2672a71d11ab6b6a48fa3a4433c2d4deb0ed435c
Edmilson <edmgold@yahoo.com.br>
2025-07-19 16:05:13 -0400

    Form input

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,24 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>Add a new task</label>
+            <input id='meuInput' type='text' placeholder='Enter a task' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 </p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
