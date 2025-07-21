cd05cc568d271f095d7ec336f85e649c4ae368f4
Edmilson <edmgold@yahoo.com.br>
2025-07-21 13:45:43 -0400

    Componente footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -41,6 +42,9 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer></Footer>
       </Container>
     </>
   );
